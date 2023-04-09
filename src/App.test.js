916888eef97abab5e5b6828f0ff3ded557f87678
JarekTkacz <116753118+JarekTkacz@users.noreply.github.com>
2023-04-09 21:37:38 +0100

    more changes345

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Test few words/i);
+  const linkElement = screen.getByText(/Test few words../i);
   expect(linkElement).toBeInTheDocument();
 });
