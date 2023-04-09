bcbfe1e2de363b9c4990a77b248df4bd87bdc328
JarekTkacz <116753118+JarekTkacz@users.noreply.github.com>
2023-04-09 22:15:34 +0100

    change12

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import logo from './logo.svg';
+import '@testing-library/jest-dom';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import NavBar from './components/NavBar';
